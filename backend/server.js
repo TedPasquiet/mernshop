1e81857687e89d812ea8e3292bdbfd13688c78cf
TedPasquiet <tpasquiet@hotmail.fr>
2023-08-10 20:01:39 +0200

    Moogoose Router

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Api is running");
+  res.send("Api is  running");
 });
 
 app.use("/api/products" , productRoutes);
