17e5a84c960b6efc9c79d003047bd5fdca6335ee
TedPasquiet <t.pasquiet@voxagrume.fr>
2023-12-22 11:27:41 +0100

    LoggedOut and clear cookies

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,6 +49,11 @@ const registerUser = asyncHandler(async (req, res) => {
 //@access Private
 
 const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "logged out successfully" });
   res.send("logout user");
 });
 
