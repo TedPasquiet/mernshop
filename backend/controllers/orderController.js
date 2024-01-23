ae101e201af9da77847fba6c89a41a9bd15c64bf
TedPasquiet <tpasquiet@hotmail.fr>
2024-01-23 23:14:01 +0100

    ecred

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -141,7 +141,7 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
 });
 
 // @desc Get  all orders
-// @route   GET /api/orders
+// @route  GET /api/orders
 // @access  Private/Admin
 const getOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find({}).populate("user", "id name");
